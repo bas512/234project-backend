86d26d1b109bf1a96c5a7c8f8b23feabafa8bc23
Unknown <p.fay@hotmail.com>
2018-05-09 19:28:41 +0700

    add method getAvailableProducts
    add method getAvailableProducts in ProductServiceImplTests class

--- src/test/java/camt/se234/project/service/ProductServiceImplTests.java
+++ src/test/java/camt/se234/project/service/ProductServiceImplTests.java
@@ -12,8 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-
-
 public class ProductServiceImplTests {
    ProductDao productDao;
    ProductServiceImpl productService;
@@ -37,4 +35,15 @@ public class ProductServiceImplTests {
       assertThat(productService.getAllProducts());
    }
 
+   @Test
+   public void getAvailableProducts(){
+      ProductServiceImpl productService = new ProductServiceImpl();
+      productService.setProductDao(productDao);
+      List<Product> mockProduct = new ArrayList<>();
+      mockProduct.add(new Product(111L,"11","Book","Book for student","image",100.00));
+      mockProduct.add(new Product(112L,"12","Pen","For write","image",25.75));
+      mockProduct.add(new Product(113L,"13","Eraser","For removing writing from paper","image",19.00));
+      when(productDao.findAll()).thenReturn(mockProduct);
+      assertThat(productService.getAvailableProducts());
+   }
 }
